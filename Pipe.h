163eadfc65dbf7ec387fc202af12401ea7f78d35
17ilsaf <ilsafsuggestions@gmail.com>
2026-09-23 20:08:53 +0300

    Initial commit

---
+++ Pipe.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class Pipe {
+    std::string name;
+    double length;
+    int diameter;
+    bool status;
+
+public:
+    std::string getName() { return name; }
+
+    bool getStatus() { return status; }
+
+    void getInfo();
+
+    void readFromConsole();
+
+    void changeStatus();
+
+    void saveToFile(std::ofstream& file);
+    void loadFromFile(std::ifstream& file);
+
+    Pipe() {
+        name = "Undefined"; length = 0.0; diameter = 0; status = false;
+    }
+};
---
+++ main.cpp
@@ -0,0 +1,209 @@
+#include "Pipe.h"
+#include "Station.h"
+#include "utils.h"
+#include <map>
+#include <chrono>
+#include <format>
+
+int main() {
+
+    redirect_output_wrapper cerr_out(std::cerr);
+
+    auto utc0 = std::chrono::system_clock::now();
+    auto utc3 = utc0 + std::chrono::hours(3);
+
+	std::string time = std::format("{:%d_%m_%Y %H_%M_%OS}", utc3);
+
+	std::ofstream logfile("logfiles/log " + time + ".txt");
+	if (logfile) { cerr_out.redirect(logfile); }
+
+    std::unordered_map<int, Pipe> pipes = {};
+    std::unordered_map<int, Station> stations = {};
+    system("clear");
+
+    int choice, pipe_choice, station_choice;
+
+    do {
+        std::cout << std::endl;
+        std::cout << "-------------Main menu-------------" << std::endl;
+        std::cout << "[1] Create new pipe" << std::endl;
+        std::cout << "[2] Create new compressor station" << std::endl;
+        std::cout << "[3] Show all objects" << std::endl;
+        std::cout << "[4] Pipes manager" << std::endl;
+        std::cout << "[5] Compressor stations manager" << std::endl;
+        std::cout << "[6] Save to file" << std::endl;
+        std::cout << "[7] Load from file" << std::endl;
+        std::cout << "[0] Exit" << std::endl;
+        std::cout << "-----------------------------------" << std::endl;
+
+        while (true) {
+            std::cout << "Choose the option: ";
+            if (validation(choice) && choice >= 0 && choice <= 7) {
+                break;
+            }
+            std::cout << std::endl << "[Error] Invalid choice! Try again!" << std::endl;
+        }
+
+        switch(choice) {
+            case 1: {
+                pipes[generateID(pipes)].readFromConsole();
+                std::cout << std::endl << "Successful creation! Return to main menu!" << std::endl;
+                break;
+            }
+            case 2: {
+                stations[generateID(stations)].readFromConsole(); 
+                std::cout << std::endl << "Successful creation! Return to main menu!" << std::endl;
+                break;
+            }
+            case 3: {           
+                if (pipes.empty()) {
+                    std::cout << std::endl << "No one pipe was created!" << std::endl; 
+                }
+                getInfo(pipes);
+
+                if (stations.empty()) {
+                    std::cout << std::endl << "No one station was created!" << std::endl; 
+                }
+                getInfo(stations);
+
+                break;
+            }
+            case 4: {
+                if (pipes.empty()) {
+                    std::cout << std::endl << "List of pipes is empty!" << std::endl;
+                    break;
+                } else {
+                    do {
+                        std::cout << std::endl;
+                        std::cout << "-------------Pipes menu-------------" << std::endl;
+                        std::cout << "[1] Filter pipes by name" << std::endl;
+                        std::cout << "[2] Filter pipes by status" << std::endl;
+                        std::cout << "[3] Change status of certain pipe" << std::endl;
+                        std::cout << "[4] Change status of all pipes" << std::endl;
+                        std::cout << "[5] Delete certain pipe" << std::endl;
+                        std::cout << "[0] Exit" << std::endl;
+                        std::cout << "-----------------------------------" << std::endl;
+
+                        while (true) {
+                            std::cout << "Choose the option: ";
+                            if (validation(pipe_choice) && pipe_choice >= 0 && pipe_choice <= 5) {
+                                break;
+                            }
+                            std::cout << std::endl << "[Error] Invalid choice! Try again!" << std::endl;
+                        }
+
+                        switch(pipe_choice) {
+                            case 1: {                                
+                                filterPipesByName(pipes);
+                                break;
+                            }
+                            case 2: {
+                                filterPipesByStatus(pipes);
+                                break;
+                            }
+                            case 3: {
+                                changePipeById(pipes);
+                                break;
+                            }
+                            case 4: {
+                                changeAllObjects(pipes);
+                                break;
+                            }
+                            case 5: {
+                                stations.clear();
+                                std::cout << std::endl << "All stations was deleted!" << std::endl;
+                                break;
+                            }
+                            case 0:
+                                break;
+                        }
+
+                        if (pipe_choice != 0) {
+                            std::cout << std::endl << "Return to pipes menu!" << std::endl;
+                        }
+
+                    } while (pipe_choice != 0);
+                }
+                break;
+            }
+            case 5:
+                if (stations.empty()) {
+                    std::cout << std::endl << "List of stations is empty!" << std::endl;
+                    break;
+                } else {
+                    do {
+                        std::cout << std::endl;
+                        std::cout << "--------------------Stations menu---------------------" << std::endl;
+                        std::cout << "[1] Filter stations by name" << std::endl;
+                        std::cout << "[2] Filter stations by count of shops in work" << std::endl;
+                        std::cout << "[3] Change count of shops in work of certain station" << std::endl;
+                        std::cout << "[4] Change count of shops in work of all stations" << std::endl;
+                        std::cout << "[5] Delete certain station" << std::endl;
+                        std::cout << "[6] Delete all stations" << std::endl;
+                        std::cout << "[0] Exit" << std::endl;
+                        std::cout << "------------------------------------------------------" << std::endl;
+
+                        while (true) {
+                            std::cout << "Choose the option: ";
+                            if (validation(station_choice) && station_choice >= 0 && station_choice <= 6) {
+                                break;
+                            }
+                            std::cout << std::endl << "[Error] Invalid choice! Try again!" << std::endl;
+                        }
+
+                        switch(station_choice) {
+                            case 1: {                                
+                                filterStationsByName(stations);
+                                break;
+                            }
+                            case 2: {
+                                filterStationsByShopsInWorkCount(stations);
+                                break;
+                            }
+                            case 3: {
+                                changeStationsById(stations);
+                                break;
+                            }
+                            case 4: {
+                                deleteObjectById(stations);
+                                break;
+                            }
+                            case 5: {
+                                deleteObjectById(stations);
+                                break;
+                            }
+                            case 6: {
+                                stations.clear();
+                                std::cout << std::endl << "All stations was deleted!" << std::endl;
+                                break;
+                            }
+                            case 0:
+                                break;
+                        }
+
+                        if (station_choice != 0) {
+                            std::cout << std::endl << "Return to stations menu!" << std::endl;
+                        }
+                    } while (station_choice != 0);
+                }
+                break;
+            case 6: {
+                saveObjectsFrom(pipes, stations);
+                break;
+            }
+            case 7: {
+                loadFromFileTo(pipes, stations);
+                break;
+            }
+            case 0:
+                break;
+        }
+
+        if (choice != 0) {
+            std::cout << std::endl << "Return to main menu!" << std::endl;  
+        } 
+
+    } while (choice != 0);
+
+    return 0;
+}
